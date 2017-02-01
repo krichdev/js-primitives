6f47b05ef17dc7ba8fdf6adad1ae2990424f7026
krichdev <kgrichardson2@gmail.com>
2017-02-01 13:41:14 -0800

    javascript lab work

---
+++ js/exercise1.js
@@ -0,0 +1,16 @@
+var friends = [
+  'Moe',
+  'Larry',
+  'Curly',
+  'Jane',
+  'Emma',
+  'Elizabeth',
+  'Elinor',
+  'Mary',
+  'Darcy',
+  'Grey',
+  'Lydia',
+  'Harriet'
+];
+
+console.log(friends[friends.length -1], "is the last friend in the list");
---
+++ js/exercise7.js
@@ -0,0 +1,27 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+//var donuts = foods.indexOf('Donuts');
+
+foods.splice(foods.indexOf('Donuts', 1));
+console.log(foods);
+//console.log(foods);
